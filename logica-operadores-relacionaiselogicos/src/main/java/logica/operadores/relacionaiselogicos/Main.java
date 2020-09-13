4b522084663c77a04ce3b3302b39aeb694a1f80b
Sandoque <acsandoque@gmail.com>
2020-09-13 03:58:40 -0300

    Primeiro commit

---
+++ logica-operadores-relacionaiselogicos/src/main/java/logica/operadores/relacionaiselogicos/Main.java
@@ -0,0 +1,20 @@
+package logica.operadores.relacionaiselogicos;
+
+import br.com.softblue.commons.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Número 1");
+		int n1 = Console.readInt();
+		
+		System.out.println("Número 2");
+		int n2 = Console.readInt();
+				
+		
+
+	}
+
+}
