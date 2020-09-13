63739e6c76b6fa12441aeecbde20174429e976f1
Sandoque <acsandoque@gmail.com>
2020-09-13 03:54:02 -0300

    Primeiro commit

---
+++ logica-variaveis/src/main/java/logica/variaveis/Main.java
@@ -0,0 +1,29 @@
+package logica.variaveis;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//int idade;
+		//idade = 20;
+		int idade = 20;
+		double nota = 8.6;
+		boolean ligado = true;
+		boolean ativo = false;
+		String nome = "Carlos";
+		
+		System.out.println(nota);
+		
+		System.out.println(nome + " -> " + nota);
+		
+		//variaveis sem ser definida o tipo antes
+		//tipo de variável reconhecido pelo conteúdo
+		var x = 5;
+		var y = "abc";
+		
+		
+		
+
+	}
+
+}
