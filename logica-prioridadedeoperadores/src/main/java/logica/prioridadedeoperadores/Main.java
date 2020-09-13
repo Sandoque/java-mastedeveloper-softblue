e936e297e1704a37adb0b08fdcff6d7af6935e44
Sandoque <acsandoque@gmail.com>
2020-09-13 16:40:15 -0300

    Testando uma expressão lógica

--- logica-prioridadedeoperadores/src/main/java/logica/prioridadedeoperadores/Main.java
+++ logica-prioridadedeoperadores/src/main/java/logica/prioridadedeoperadores/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		boolean v = true;
+		
+		boolean x = 5 * 5 + Math.sqrt(7 * 7 ) <= 100 || !v && v;
+		System.out.println(x);
+		
+		
 
 	}
 
