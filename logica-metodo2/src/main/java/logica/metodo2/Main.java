0988a4188c62ed8b966150bf87cecd67d70663eb
Sandoque <acsandoque@gmail.com>
2020-09-17 04:21:00 -0300

    Conversor de temperatura

--- logica-metodo2/src/main/java/logica/metodo2/Main.java
+++ logica-metodo2/src/main/java/logica/metodo2/Main.java
@@ -1,10 +1,40 @@
 package logica.metodo2;
 
+import br.com.softblue.commons.io.Console;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.print("Digite a temperatura: ");
+		double temp = Console.readDouble();
+
+		System.out.print("Qual a conversão? (1 = F -> C, 2 = C -> F): ");
+		int opcao = Console.readInt();
+
+		double r;
+
+		if (opcao == 1) {
+			r = converterParaCelsius(temp);
+
+		} else if (opcao == 2) {
+			r = converterParaFahrenheit(temp);
+
+		} else {
+			System.out.println("Opção inválida");
+			return;
+		}
+
+		System.out.println("A temperatura convertida é: " + r);
+	}
+
+	static double converterParaCelsius(double fahrenheit) {
+		return (fahrenheit - 32) / 1.8;
+	}
+
+	static double converterParaFahrenheit(double celsius) {
+		return celsius * 1.8 + 32;
+	}
 
 	}
 
-}
+
