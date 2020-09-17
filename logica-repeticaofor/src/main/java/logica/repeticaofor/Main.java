c3d6aea1f1af4588b72642fe5498660fddd8d2d3
Sandoque <acsandoque@gmail.com>
2020-09-17 02:22:36 -0300

    Primeiro Commit

---
+++ logica-repeticaofor/src/main/java/logica/repeticaofor/Main.java
@@ -0,0 +1,25 @@
+package logica.repeticaofor;
+
+import br.com.softblue.commons.io.Console;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x;
+		for (x = 0; x <= 10; x++) {
+			System.out.println(x);
+		}
+		System.out.println(x);
+		
+		System.out.println("Digite um número:");
+		int n = Console.readInt();
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(n + " X " + i + " = " + (n * i));
+		}
+		
+		
+
+	}
+
+}
