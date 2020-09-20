7c6f90d5b4f54e1eba6a61a55f5ed4413d517e03
Sandoque <acsandoque@gmail.com>
2020-09-20 14:52:58 -0300

    Criando as classes Player, Move, Board e Constants

--- tictactoe/src/main/java/br/com/softblue/tictactoe/core/Game.java
+++ tictactoe/src/main/java/br/com/softblue/tictactoe/core/Game.java
@@ -2,8 +2,12 @@ package br.com.softblue.tictactoe.core;
 
 public class Game {
 	
+	Board board = new Board();
+	Player[] players= new Player[(Constants.SYMBOL_PLAYERS.length)];
+	
+	
 	public void play() {
-		System.out.println("Jogando...");
+		
 	}
 
 }
---
+++ tictactoe/src/main/java/br/com/softblue/tictactoe/core/Move.java
@@ -0,0 +1,9 @@
+package br.com.softblue.tictactoe.core;
+
+public class Move {
+	
+	int i;
+	int j;
+	
+
+}
