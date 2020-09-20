1d012604a1022f20e5a021e0ca35a4e1b813a276
Sandoque <acsandoque@gmail.com>
2020-09-20 14:59:23 -0300

    Revert "Revert "Criando as classes Player, Move, Board e Constants""
    This reverts commit 87e61879c704483043cea71daa89d4f0f1baad96.

---
+++ tictactoe/src/main/java/br/com/softblue/tictactoe/core/Player.java
@@ -0,0 +1,18 @@
+package br.com.softblue.tictactoe.core;
+
+public class Player {
+	
+	String name;
+	Board board;
+	char symbol;
+	
+	Move inputMove() {
+		return null;
+	}
+	
+	void play() {
+		
+	}
+	
+
+}
