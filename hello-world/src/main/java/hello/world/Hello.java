768acfe09a00f35b8f265487a8870165cbcd87c3
Sandoque <acsandoque@gmail.com>
2020-10-02 06:58:58 -0300

    incrementos caixa bit

--- hello-world/src/main/java/hello/world/Hello.java
+++ hello-world/src/main/java/hello/world/Hello.java
@@ -5,7 +5,25 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, world!");
+		
+		int x = 13; 
+		System.out.println("int x = 13 >> X: " + x);
+		
+		x+=8;
+		System.out.println("x+=8 >> X: " + x);
+		
+		--x;
+		System.out.println("--x >> X: " + x);
+		
+		x=7+x;
+		System.out.println("7+x >> X: " + x);
+		
+		x/=9;
+		System.out.println("x/=9 >> X: " + x);
+		
+		
+			
+		};
 
 	}
 
-}
--- logica-decisaoif/src/main/java/logica/decisaoif/Main1.java
+++ logica-decisaoif/src/main/java/logica/decisaoif/Main1.java
@@ -1,33 +1,86 @@
 package logica.decisaoif;
 
+
 import br.com.softblue.commons.io.Console;
 
 public class Main1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Digite a idade:");
-		int idade = Console.readInt();
+		System.out.println("Digite valor:");
+		int valor = Console.readInt();
+		int n50=0, n10=0, n5=0, n1=0;
 		
-		if (idade >= 0 && idade <= 12) {
-			System.out.println("Você é criança");
+		
+		if (valor > 0 && valor <= 1000) {
+	 		while (valor > 0) {
+				
+				if (valor >= 50) {
+					valor -=50;
+					n50++;
+					
+				}
+				
+				else if (valor >= 10) {
+					valor -=10;
+					n10++;
+					
+				}
+				
+	
+				else if (valor >= 5) {
+					valor -=5;
+					n5++;
+					
+				}
+	
+				else if (valor < 5) {
+					valor -=1;
+					n1++;
+					
+				}
+				
+				
+			}
+		
+	 		System.out.println("Aí está: ");
+			if (n50 > 0) {
+				if (n50 == 1) {
+					System.out.println(n50 + " nota de 50 Bit");
+				} else {
+					System.out.println(n50 + " notas de 50 Bits");
+				}
+			}
 			
-		} else if (idade > 12 && idade <= 17) {
-			System.out.println("Você é adolescente");
+			if (n10 > 0) {
+				if (n10 == 1) {
+					System.out.println(n10 + " nota de 10 Bit");
+				} else {
+					System.out.println(n10 + " notas de 10 Bits");
+				}
+			}
 			
-		} else if (idade > 17 && idade <= 59) {
-			System.out.println("Você é adulto");
+			if (n5 > 0) {
+				if (n5 == 1) {
+					System.out.println(n5 + " nota de 5 Bit");
+				} else {
+					System.out.println(n5 + " notas de 5 Bits");
+				}
+			}
+				
+			if (n1 > 0) {
+				if (n1 == 1) {
+					System.out.println(n1 + " nota de 1 Bit");
+				} else {
+					System.out.println(n1 + " notas de 1 Bits");
+				}
+			}
 			
-		} else if (idade > 59) {
-			System.out.println("Você é idoso");
-			
-		} else {
-			System.out.println("Você digitou idade inválida");
+		
 		}
 		
-		System.out.println("Fim do programa");
-		
-
+	
 	}
+	
 
 }
